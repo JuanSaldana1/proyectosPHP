e6c1f1ac18f7d3e3cbdcf9cd7b336495a4f12fad
JuanSaldana1 <72858838+JuanSaldana1@users.noreply.github.com>
2020-12-18 09:12:28 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>1st PHP Page</title>
+</head>
+
+<body>
+    <?php echo '<p>Hello World</p>';
+    echo "Hello World!<br>";
+    echo "Hello World!<br>";
+    echo "Hello World!<br>";
+    ?>
+    <?php
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " . $coLOR . "<br>";
+    ?>
+</body>
+
+</html>
